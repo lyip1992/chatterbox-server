a77d11d50a11c394b9316957e899404b1184accf
lyip1992 <lyip1992@gmail.com>
2015-06-01 16:26:28 -0700

    Passed all mocha tests, working on chatterbox itself

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,7 +11,8 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
-
+var url = require('url');
+var resultsArray = [];
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
   //
@@ -29,21 +30,66 @@ var requestHandler = function(request, response) {
   // console.logs in your code.
   console.log("Serving request type " + request.method + " for url " + request.url);
 
-  // The outgoing status.
-  var statusCode = 200;
-
+  var pathname = url.parse(request.url).pathname;
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
+  var statusCode;
+
+  if (pathname === '/classes/messages') {
+    if( request.method === 'GET') {
+      statusCode = 200;
+      // The outgoing status.
+      headers['Content-Type'] = "application/json";
+      response.writeHead(statusCode, headers);
+      //response.end(response._data);
+      response.end(JSON.stringify({results: resultsArray}));
+    } else if( request.method === 'POST') {
+      statusCode = 201;
+      // The outgoing status.
+      headers['Content-Type'] = "application/json";
+      response.writeHead(statusCode, headers);
+
+      request.on('data', function(data) {
+        response.end(data);
+        resultsArray.push(JSON.parse(data));
+      });
+
+    }
+  } else if ((/\/classes\/.*/).test(pathname)) {
+    if( request.method === 'GET') {
+      statusCode = 200;
+      // The outgoing status.
+      headers['Content-Type'] = "application/json";
+      response.writeHead(statusCode, headers);
+      //response.end(response._data);
+      response.end(JSON.stringify({results: resultsArray}));
+    } else if( request.method === 'POST') {
+      statusCode = 201;
+      // The outgoing status.
+      headers['Content-Type'] = "application/json";
+      response.writeHead(statusCode, headers);
+      request.on('data', function(data) {
+        response.end(data);
+        resultsArray.push(JSON.parse(data));
+      });
+      // response.end();
+    }
+  } else {
+    statusCode = 404;
+    response.writeHead(statusCode, headers);
+    response.end();
+  }
+
+
 
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = "text/plain";
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  // response.write(JSON.stringify(request));
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +98,6 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end("Hello, World!");
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -70,4 +115,6 @@ var defaultCorsHeaders = {
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10 // Seconds.
 };
+
+module.exports.requestHandler = requestHandler;
 
